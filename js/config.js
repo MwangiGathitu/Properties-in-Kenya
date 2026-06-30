e19beb092dc4677f5d87ff532512f3aad2534467
MwangiGathitu <mwangigathitu40@gmail.com>
2026-06-30 10:45:12 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-const supabaseClient = supabase.createClient(
+window.supabaseClient = supabase.createClient(
     "https://nqwvsmuvltbiekfnvovx.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xd3ZzbXV2bHRiaWVrZm52b3Z4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1NjU4MDAsImV4cCI6MjA5NjE0MTgwMH0.Xrc-bbAuWdvKSPHnVhTaLiQphV61xeYtDepWePqsrdo"
 );
